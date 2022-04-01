275593498b1a0006a21be4472006f2dac62b9777
stanislav.radchenko <stanislav.radchenko@effective.band>
2022-04-02 01:59:17 +0600

    - Chat list screen update

--- app/src/main/java/stanislav/radchenko/worldcinema/screens/chatlist/ChatListScreen.kt
+++ app/src/main/java/stanislav/radchenko/worldcinema/screens/chatlist/ChatListScreen.kt
@@ -1,20 +1,30 @@
 package stanislav.radchenko.worldcinema.screens.chatlist
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.ContentAlpha
 import androidx.compose.material.Divider
+import androidx.compose.material.LocalContentAlpha
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.koin.getScreenModel
@@ -22,6 +32,7 @@ import stanislav.radchenko.worldcinema.R
 import stanislav.radchenko.worldcinema.network.model.response.ChatResponse
 import stanislav.radchenko.worldcinema.ui.common.ErrorScreenState
 import stanislav.radchenko.worldcinema.ui.common.LoadingScreenState
+import stanislav.radchenko.worldcinema.ui.common.TopBarWithBackButton
 
 class ChatListScreen : Screen {
     @Composable
@@ -40,28 +51,54 @@ class ChatListScreen : Screen {
 
     @Composable
     fun ChatList(list: List<ChatResponse>, onChatClick: (String) -> Unit) {
-        LazyColumn(content = {
-            items(list) { chat ->
-                ChatListItem(chat, onChatClick)
-            }
-        })
+        Scaffold(topBar = {
+            TopBarWithBackButton(title = stringResource(id = R.string.discussion))
+        }) {
+            LazyColumn(content = {
+                items(list) { chat ->
+                    ChatListItem(chat, onChatClick)
+                }
+            })
+        }
     }
 
     @Composable
     fun ChatListItem(chat: ChatResponse, onChatClick: (String) -> Unit) {
-        Row(
+        Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp)
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.no_image_profile_placeholder),
-                contentDescription = null
-            )
-            Spacer(modifier = Modifier.width(16.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.chat_image_example),
+                    contentDescription = null,
+                    modifier = Modifier.size(64.dp)
+                )
+                Spacer(modifier = Modifier.width(16.dp))
 
-            Text(text = chat.name)
-            Divider()
+                Column() {
+                    Text(text = chat.name)
+                    Row() {
+                        Text(buildAnnotatedString {
+                            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                                append("Иван: ")
+                            }
+                            append("Смотрели уже последнюю серию? Я просто поверить не...")
+                        })
+                    }
+                }
+            }
+            Divider(
+                color = Color.Gray,
+                thickness = 3.dp,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(1.dp)
+            )
         }
     }
 }
